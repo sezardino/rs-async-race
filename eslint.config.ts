655dbb33d463cc7b4d176a9a8ec598093892bbb7
Edward <e.arechwa@gmail.com>
2025-03-26 19:27:29 +0100

    feat: add json server to project

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
-  { ignores: ['src/utils/object.ts'] },
+  { ignores: ['server.js'] },
   {
     linterOptions: {
       noInlineConfig: true,
